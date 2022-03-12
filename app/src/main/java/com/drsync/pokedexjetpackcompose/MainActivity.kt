3741e023ac7750558e7147248fc0d83f7f517fd4
Muhammad Beni Fajri <67430854+bennyfajri@users.noreply.github.com>
2022-03-12 21:37:40 +0700

    setting up navigation and hilt

--- app/src/main/java/com/drsync/pokedexjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/drsync/pokedexjetpackcompose/MainActivity.kt
@@ -8,36 +8,59 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.drsync.pokedexjetpackcompose.ui.theme.PokedexJetpackComposeTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             PokedexJetpackComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                val navController = rememberNavController()
+                NavHost(
+                    navController = navController,
+                    startDestination = LIST_SCREEN
                 ) {
-                    Greeting("Android")
+                   composable(LIST_SCREEN) {
+
+                   }
+                    composable(
+                        "$DETAIL_SCREEN/{$DOMINANT_COLOR}/{$POKEMON_NAME}",
+                        arguments = listOf(
+                            navArgument(DOMINANT_COLOR) {
+                                type = NavType.IntType
+                            },
+                            navArgument(POKEMON_NAME) {
+                                type = NavType.StringType
+                            }
+                        )
+                    ) {
+                        val dominantColor = remember {
+                            val color = it.arguments?.getInt(DOMINANT_COLOR)
+                            color?.let { Color(it) } ?: Color.White
+                        }
+                        val pokemonName = remember {
+                            it.arguments?.getString(POKEMON_NAME)
+                        }
+                    }
                 }
             }
         }
     }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    PokedexJetpackComposeTheme {
-        Greeting("Android")
+    companion object {
+        const val LIST_SCREEN = "pokemon_list_screen"
+        const val DETAIL_SCREEN = "pokemon_detail_screen"
+        const val DOMINANT_COLOR = "dominantColor"
+        const val POKEMON_NAME = "pokemonName"
     }
 }
---
+++ app/src/main/java/com/drsync/pokedexjetpackcompose/di/AppModule.kt
@@ -0,0 +1,10 @@
+package com.drsync.pokedexjetpackcompose.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+}
